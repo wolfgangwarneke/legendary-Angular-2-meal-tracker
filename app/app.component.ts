f97ec6be1da84eb3e38c618d5a169fdf270d70c8
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-08-12 09:11:27 -0700

    initial filetree created

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  directives: [],
+  template: `
+    <div class="container">
+      <h1>Meal Tracker</h1>
+
+    <div>
+  `
+})
+export class AppComponent {
+
+}
