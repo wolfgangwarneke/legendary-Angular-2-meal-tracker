203d936cbc85cb9e2a76fa5c6352fad3442e0e4e
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-08-12 09:21:17 -0700

    create hard-coded meal and display food name to page

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,10 +6,19 @@ import { Component, EventEmitter } from 'angular2/core';
   template: `
     <div class="container">
       <h1>Meal Tracker</h1>
-
+      <h3>{{ meal.food }}<h3>
     <div>
   `
 })
 export class AppComponent {
+  public meal: Meal;
+  constructor(){
+    this.meal = new Meal("hamburger", "so good", 900, 0);
+  }
+}
 
+export class Meal {
+  constructor(public food: string, public notes: string, public calories: number, public id: number) {
+
+  }
 }
